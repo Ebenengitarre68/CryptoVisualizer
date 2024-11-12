79f67abbe66b0e8695771e02814fde163c6cd18f
Daniel Holzapfel <68915353+Ebenengitarre68@users.noreply.github.com>
2024-11-12 16:54:02 +0100

    Added Loading Graphs from Server

--- src/App.tsx
+++ src/App.tsx
@@ -50,144 +50,28 @@ const defaultEdgeOptions = {
 
 const initNodes: MyNode[] = [
   {
-    id: 'c1',
-    type: 'comment',
-    data: {
-      text: 'Hier könnte ihre Werbung stehen'
-    },
-    position: {x: 0, y: -100 },
-  },
-  {
     id: 't1',
     type: 'text',
     data: {
-      text: 'hello'
+      text: 'Hello World'
     },
     position: { x: 0, y: 0 },
-  },
-  {
-    id: 't2',
-    type: 'text',
-    data: {
-      text: 'tothe'
-    },
-    position: { x: 200, y: 0 },
-  },
-  {
-    id: 't3',
-    type: 'text',
-    data: {
-      text: 'world'
-    },
-    position: { x: 400, y: 0 },
-  },
-  {
-    id: '2b1',
-    type: 't2binary',
-    data: {
-      bytes: [0]
-    },
-    position: { x: 50, y: 100 },
-  },
-  {
-    id: '2b2',
-    type: 't2binary',
-    data: {
-      bytes: [0]
-    },
-    position: { x: 250, y: 100  },
-  },
-  {
-    id: '2b3',
-    type: 't2binary',
-    data: {
-      bytes: [0]
-    },
-    position: { x: 450, y: 100 },
-  },
-  {
-    id: 'x1',
-    type: 'xor',
-    data: {
-      bytes: [0]
-    },
-    position: { x: 250, y: 200 },
   },
   {
     id: 'r1',
     type: 'monitor',
     data: {},
-    position: { x: 0, y: 300 },
+    position: { x: 38, y: 100 },
   },
-  {
-    id: 'r2',
-    type: 'monitor',
-    data: {},
-    position: { x: 200, y: 300 },
-  },
+
 ];
 
 const initEdges: Edge[] = [
   {
-    id: 't1-2b1',
+    id: 't1-r1',
     source: 't1',
     sourceHandle:'text',
-    target: '2b1',
-  },
-  {
-    id: 't2-2b2',
-    source: 't2',
-    sourceHandle:'text',
-    target: '2b2',
-  },
-  {
-    id: 't3-2b3',
-    source: 't3',
-    sourceHandle:'text',
-    target: '2b3',
-  },
-  {
-    id: '2b1-x1',
-    source: '2b1',
-    sourceHandle:'bytes',
-    target: 'x1',
-  },
-  {
-    id: '2b2-x1',
-    source: '2b2',
-    sourceHandle:'bytes',
-    target: 'x1',
-    type:'display',
-  },
-  {
-    id: '2b3-x1',
-    source: '2b3',
-    sourceHandle:'bytes',
-    target: 'x1',
-  },
-  {
-    id: '2b1-r1',
-    source: '2b1',
-    sourceHandle:'bytes',
     target: 'r1',
-  },
-  {
-    id: '2b2-r1',
-    source: '2b2',
-    sourceHandle:'bytes',
-    target: 'r1',
-  },
-  {
-    id: '2b3-r1',
-    source: '2b3',
-    sourceHandle:'bytes',
-    target: 'r1',
-  },
-  {
-    id: 'x1-r2',
-    source: 'x1',
-    sourceHandle:'bytes',
-    target: 'r2',
   },
 ];
 
@@ -195,7 +79,7 @@ let id = initNodes.length;
 const getId = () => `node_${id++}`;
 
 const CustomNodeFlow = () => {
-  const [colorMode, setColorMode] = useState<ColorMode>('light');
+  const [colorMode] = useState<ColorMode>('light');
   const reactFlowWrapper = useRef(null);
   const [nodes,setNodes , onNodesChange] = useNodesState(initNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initEdges);
@@ -217,8 +101,7 @@ const CustomNodeFlow = () => {
     event.dataTransfer.dropEffect = 'move';
   }, []);
 
-  const onEdgeContextMenu = useCallback(
-      (event, edge) => {
+  const onEdgeContextMenu = useCallback((event, edge) => {
         // Prevent native context menu from showing
         event.preventDefault();
 
@@ -237,8 +120,7 @@ const CustomNodeFlow = () => {
       [setMenu],
   )
 
-  const onDrop = useCallback(
-      (event) => {
+  const onDrop = useCallback( (event) => {
         event.preventDefault();
 
         // check if the dropped element is valid
@@ -267,7 +149,7 @@ const CustomNodeFlow = () => {
   const onSave = useCallback(() => {
     if (rfInstance) {
       const flow = rfInstance.toObject();
-      const blob = new Blob([JSON.stringify(flow)],{type: 'text/plain'});
+      const blob = new Blob([JSON.stringify(flow)],{type: 'text/json'});
       const url = window.URL.createObjectURL(blob);
       const link = document.createElement("a");
       link.download = "flow.json";
@@ -278,16 +160,40 @@ const CustomNodeFlow = () => {
 
   const onEmptyNew = useCallback(() => {
     const restoreFlow = async () => {
-      const x = 0;
-      const y = 0;
-      const zoom = 1 ;
-      setNodes([]);
-      setEdges([]);
+      const x = 320;
+      const y = 115;
+      const zoom = 2 ;
+      setNodes(initNodes);
+      setEdges(initEdges);
       setViewport({ x, y, zoom });
     };
-
+    document.getElementById("algChanger").value ="default"
     restoreFlow();
   }, [setNodes, setViewport]);
+
+  const onRestore = useCallback((path:string) => {
+
+    const restoreFlow = async (json:string) => {
+      const flow = json;
+      if (flow) {
+        const {x = 0, y = 0, zoom = 1} = flow.viewport;
+        setNodes(flow.nodes || []);
+        setEdges(flow.edges || []);
+        setViewport({x, y, zoom});
+      }
+    };
+    fetch(path)
+        .then(response =>
+          response.json()
+        ).then(json => restoreFlow(json)
+    )
+  }, [setNodes, setViewport]);
+
+  const onAlgChange: ChangeEventHandler<HTMLSelectElement> = (evt) => {
+    if(evt.target.value !== 'default'){
+      onRestore(evt.target.value)
+    }
+  }
 
   return (
       <div className="dndflow" onClick={onPaneClick}>
@@ -315,12 +221,12 @@ const CustomNodeFlow = () => {
             <Background/>
             {menu && <ContextMenu {...menu}/>}
             <Panel>
-              <div className="nav-panel">
+              <div>
                 <button className="nav-button left-nav-button" onClick={onEmptyNew}>Empty</button>
                 <button className="nav-button" onClick={onSave}>Download</button>
-                <select name="algorithm" className="nav-button right-nav-button">
-                  <option selected  hidden>Select Algorithm</option>
-                  <option>AES 128</option>
+                <select id="algChanger" onChange={onAlgChange}  className="nav-button right-nav-button" defaultValue="default">
+                  <option value="default" hidden>Select Algorithm</option>
+                  <option value="/graphs/test.json" >Test</option>
                 </select>
 
               </div>
--- src/nodes/CommentNode.tsx
+++ src/nodes/CommentNode.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useEffect,useState } from 'react';
+import React, { memo } from 'react';
 import {
     type NodeProps,
     type Node, useReactFlow,

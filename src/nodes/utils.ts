052c3bce697a8f2e6d21f4f7936ef57d3ac1b257
Daniel Holzapfel <68915353+Ebenengitarre68@users.noreply.github.com>
2024-11-15 13:43:35 +0100

    Added Ranom Gen to utils

--- src/nodes/utils.ts
+++ src/nodes/utils.ts
@@ -1,6 +1,7 @@
 import { type Node } from '@xyflow/react';
 
 
+
 export type TextNode = Node<{}, 'text'>;
 export type MonitoringNode = Node<{}, 'monitor'>;
 export type CommentNode = Node<{text: string}, 'comment'>;
@@ -8,8 +9,9 @@ export type TextToBinaryNode = Node<{}, 't2binary'>;
 export type XorNode = Node<{}, "xor">;
 export type BinaryToText = Node<{}, "b2text">;
 export type SplitNode = Node<{outputs: number}, "split">;
+export type RandomGenNode = Node<{}, "random">;
 
-export type MyNode = TextNode | MonitoringNode | TextToBinaryNode | XorNode | CommentNode | BinaryToText | SplitNode;
+export type MyNode = TextNode | MonitoringNode | TextToBinaryNode | XorNode | CommentNode | BinaryToText | SplitNode | RandomGenNode;
 
 export function isTextNode(
     node: any,

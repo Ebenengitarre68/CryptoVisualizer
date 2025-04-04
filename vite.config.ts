dfd8ae38d6a2b18a375a467fc26fb8f38f15b9e7
Daniel Holzapfel <68915353+Ebenengitarre68@users.noreply.github.com>
2025-04-04 17:36:21 +0200

    configured to run on my domain

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base:'/KryptoVisualizer/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
